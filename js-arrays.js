699b324bed877b01c0cc2b68eb06ddcdcc3c796f
yaelBrown <yaelrbrown@gmail.com>
2019-07-20 10:55:45 -0500

    added js arrays lesson file

---
+++ js-arrays.js
@@ -0,0 +1,96 @@
+// let cars = ['mazda', 'honda', 'ford']
+
+// console.log(cars)
+
+// let cars = []
+// cars.push('mazda')
+// cars.push('honda')
+// cars.push('ford')
+
+// console.log(cars)
+
+// let cars = []
+// cars[0] = 'mazda'
+// cars[1] = 'honda'
+// cars[2] = 'ford'
+
+// console.log(cars)
+
+// let cars = []
+// cars[1] = 'mazda'
+// cars[4] = 'honda'
+// cars[9] = 'ford'
+
+// console.log(cars)
+
+// let cars = []
+// cars['1'] = 'mazda'
+// cars['4'] = 'honda'
+// cars['9'] = 'ford'
+
+// console.log(cars)
+
+// let cars = {}
+// cars['1'] = 'mazda'
+// cars['4'] = 'honda'
+// cars['9'] = 'ford'
+
+// console.log(cars)
+
+// Practice: Make an array of your favorite bands/musicians.  Populate it using all three methods of adding array elements.  Put a bunch in there!
+// 3 minutes
+
+// let cars = ['mazda', 'honda', 'ford']
+
+// cars.pop()
+
+// console.log(cars)
+
+// cars.shift()
+
+// console.log(cars)
+
+// cars.splice(1, 1)
+
+// console.log(cars)
+
+// delete cars[1]
+
+// console.log(cars)
+
+// Practice: Delete some bands using a mix of methods.
+// 3 minutes
+
+// let hondaIndex = cars.indexOf('honda')
+
+// console.log(hondaIndex)
+
+// let hondaIndex = cars.findIndex(car => car === 'honda')
+
+// console.log(hondaIndex)
+
+// let hasHonda = hondaIndex >= 0
+
+// console.log(hasHonda)
+
+// let hasHonda = !!cars.find(car => car === 'honda')
+
+// console.log(hasHonda)
+
+// let hasHonda = cars.filter(car => car === 'honda').length > 0
+
+// console.log(hasHonda)
+
+// Practice: Search for specific bands in your array, using a mix of methods.
+
+// let screaminCars = cars.map(car => car.toUpperCase())
+
+// console.log(screaminCars)
+
+// let screaminCars = []
+// cars.forEach(car => screaminCars.push(car.toUpperCase()))
+
+// console.log(screaminCars)
+
+// Practice: Modify your bands array to add some fun decorative characters like asterixes, using the methods described.
+// 5 minutes
